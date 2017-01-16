08a053227238ba37a60d1fa9171c635b17da52b9
Seiya Loveless <seiya-l@ya.ru>
2017-01-16 17:32:50 +0300

    1.0.3

--- sei-helper.js
+++ sei-helper.js
@@ -1,30 +1,27 @@
 module.exports = {
 	numberFormat: function(number, decimals, decPoint, thousandsSep) {
-		decimals = isNaN(decimals) ? 2 : Math.abs(decimals);
+		decimals = isNaN(decimals) ? 1 : Math.abs(decimals);
 		decPoint = (decPoint === undefined) ? '.' : decPoint;
 		thousandsSep = (thousandsSep === undefined) ? '' : thousandsSep;
-		
 		var sign = number < 0 ? '-' : '';
 		number = Math.abs(+number || 0);
-		
 		var intPart = parseInt(number.toFixed(decimals), 10) + '';
 		var j = intPart.length > 3 ? intPart.length % 3 : 0;
-		
 		return ((''
 			)+( sign
 			)+( (j ? intPart.substr(0, j) + thousandsSep : '')
 			)+( intPart.substr(j).replace(/(\d{3})(?=\d)/g, '$1' + thousandsSep)
-			)+( (decimals ? decPoint + Math.abs(number - intPart).toFixed(decimals).slice(2) : '')
+			)+( (decimals ? decPoint + Math.abs(number - intPart).toFixed(decimals).slice(1) : '')
 		));
 	},
-	hsize: function(value){
+	hsize: function(value, decimals){
 		var units = [[1, 'B'], [1024, 'KiB'], [1024*1024, 'MiB'], [1024*1024*1024, 'GiB']];
 		for (var i=0; i<units.length-1; i++) {
 			if (value < units[i+1][0]) {
-				return this.numberFormat(value / units[i][0], 2) + units[i][1];
+				return this.numberFormat(value / units[i][0], decimals) + units[i][1];
 			}
 		}
-		return this.numberFormat(value / units[units.length-1][0], 2) + units[units.length-1][1];
+		return this.numberFormat(value / units[units.length-1][0], decimals) + units[units.length-1][1];
 	},
 	htime: function(value){
 		var sec_num = parseInt(value, 10);
@@ -82,5 +79,45 @@ module.exports = {
 			result.push(p.substring(start));
 		}
 		return result;
+	},
+	cleanupFilename: function(flnm){
+		var fixingChar = '_';
+		var illegalRe = /[\/\?<>\\:\*\|":]/g; // Illegal Characters on Various Operating Systems: / ? < > \ : * | "
+		var controlRe = /[\x00-\x1f\x80-\x9f]/g; // Unicode Control codes: C0 0x00-0x1f & C1 (0x80-0x9f)
+		var reservedRe = /^\.+$/; // Reserved filenames on Unix-based systems (".", "..")
+		var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i; 
+		/*	Reserved filenames in Windows ("CON", "PRN", "AUX", "NUL", "COM1",
+			"COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+			"LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", and
+			"LPT9") case-insesitively and with or without filename extensions. */
+		var windowsTrailingRe = /[\. ]+$/;
+		flnm = flnm
+				.replace(illegalRe, fixingChar)
+				.replace(controlRe, fixingChar)
+				.replace(reservedRe, fixingChar)
+				.replace(windowsReservedRe, fixingChar)
+				.replace(windowsTrailingRe, fixingChar);
+		return flnm;
+	},
+	parseDate: function(date,notime){
+		if(date === 0){
+			return '0000-00-00 00:00:00 UTC';
+		}
+		date = new Date(date);
+		var year = date.getUTCFullYear();
+		var mnth = date.getUTCMonth() + 1;
+			mnth = mnth < 10 ? '0'+mnth : mnth;
+		var dtnp = date.getUTCDate();
+			dtnp = dtnp < 10 ? '0'+dtnp : dtnp;
+		var hour,mins,secs;
+		if(!notime){
+			hour = date.getUTCHours();
+			hour = hour < 10 ? '0'+hour : hour;
+			mins = date.getUTCMinutes();
+			mins = mins < 10 ? '0'+mins : mins;
+			secs = date.getUTCSeconds();
+			secs = secs < 10 ? '0'+secs : secs;
+		}
+		return year+'-'+mnth+'-'+dtnp+(notime?'':' '+hour+':'+mins+':'+secs)+' '+'UTC';
 	}
 };
