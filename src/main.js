dc5e30ea8a597a6f9a8baacbceff7a18e8c3517e
Seiya <seiya-git@users.noreply.github.com>
2018-06-15 16:15:19 +0300

    move cookie code, fix question module

--- src/main.js
+++ src/main.js
@@ -150,50 +150,27 @@ const validateIpAndPort = (input) => {
     });
 }
 
-const makeCookie = (data, keys) => {
-    let res = [];
-    for (let key of keys) {
-        if (typeof data[key] !== 'object') continue;
-        res.push(`${key}=${data[key].value}`);
-    }
-    return res.join('; ');
-};
-
-const parseCookie = (data) => {
-    let res = {};
-    for (let line of data) {
-        let c = line.split('; ');
-        let val = c.shift().split('=');
-        res[val[0]] = {
-            value: val[1]
-        };
-        for (let f of c) {
-            let param = f.split('=');
-            if (param[0].toLowerCase() === 'expires') {
-                res[val[0]].expires = new Date(param[1]);
-            } else if (param[1] === undefined) {
-                res[val[0]][param[0]] = true;
-            } else {
-                res[val[0]][param[0]] = param[1];
-            }
-        }
-    }
-    return res;
-};
-
 // extra modules
 
-const question = require('./question');
+const cookie       = require('./module.cookie');
+const { question } = require('./module.question');
 
 // export
 
 module.exports = {
     // main list
-    numberFormat, hsize, htime, 
-    typeOfSN, updLn, uplStatus, 
-    parseWinCmdLineParam, cleanupFilename, 
-    parseDate, exec, validateNum, validateIpAndPort, 
-    makeCookie, parseCookie,
+    numberFormat,
+    hsize,
+    htime, 
+    typeOfSN,
+    updLn,
+    uplStatus, 
+    parseWinCmdLineParam,
+    cleanupFilename,
+    parseDate, exec,
+    validateNum,
+    validateIpAndPort, 
     // extra list
+    cookie,
     question
 };
