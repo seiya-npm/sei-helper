fe688d20798546398e3bfcc2d28f2c8f2428eb5c
Seiya <seiya.loveless.mail@gmail.com>
2019-10-27 12:11:55 +0300

    3.3.0

--- src/module.ask.js
+++ src/module.ask.js
@@ -2,6 +2,9 @@ const ask = async function(arr){
     try{
         require.resolve('inquirer');
         const inquirer = require('inquirer');
+        if(!arr.name){
+            arr.name = Math.random().toString(36).substring(8);
+        }
         arr.message += `:`;
         const result = await inquirer.prompt(arr);
         return result[arr.name];
